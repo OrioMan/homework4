0bfdaf9fe7babcdc469ac63823fb4ee60a8c8304
OrioMan <sielsan1@gmail.com>
2024-07-27 15:15:46 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#Организуйте программу
+my_string = input("Введите ваше слово: ")
+print('Длина строки:', len(my_string))
+#Работа с методами строк:Используя методы строк, выполните следующие действия:
+my_string = 'Я учусь в Urban'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
