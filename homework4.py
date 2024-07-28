bfaa5f353ff030f9cb84f486e8da56b4748b9e76
OrioMan <sielsan1@gmail.com>
2024-07-28 22:43:38 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -2,7 +2,6 @@
 my_string = input("Введите ваше слово: ")
 print('Длина строки:', len(my_string))
 #Работа с методами строк:Используя методы строк, выполните следующие действия:
-my_string = 'Я учусь в Urban'
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(' ',''))
